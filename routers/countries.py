123f06db2aee0882df4934081f3f2ef53b69019a
Magda Slifierz <m.slifierz@gmail.com>
2023-09-08 21:48:47 -0700

    get-country

--- routers/countries.py
+++ routers/countries.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from models import Country
 from schemas import CountryCreate, ShowCountry
 from sqlalchemy.orm import Session
@@ -39,3 +39,12 @@ def create_country(country: CountryCreate, db: Session = Depends(get_db)):
     #     db.rollback()
     #     raise HTTPException(status_code=500, detail="Internal Server Error")
 
+#passing in path parameter router the id of the element what we want to read
+@router.get('/country/{country_id_pass}', tags=['countries'], response_model=ShowCountry)
+def get_country_by_id(country_id_pass: int, db: Session = Depends(get_db)): #i will need db because I get the id from db
+    # I have to query data
+    country = db.query(Country).filter(Country.country_id==country_id_pass).first() #country_id_pass that is what user pass into url
+    if not country:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Country does not exist. Create it")
+    return country
+    
