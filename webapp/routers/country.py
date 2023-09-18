ab14a869d8639b884e973787ec63b549b5657ed4
Magda Slifierz <m.slifierz@gmail.com>
2023-09-18 13:00:33 -0700

    home

--- webapp/routers/country.py
+++ webapp/routers/country.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from models import Country
+from sqlalchemy.orm import Session
+from database import get_db
 
 
 router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request" : request})
+def home(request: Request, db: Session=Depends(get_db)):
+    countries = db.query(Country).all()
+    return templates.TemplateResponse("home.html", {"request" : request, "countries": countries})
