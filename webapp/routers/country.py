8eae5774408bab3df4e152b04f22f401d236495e
Magda Slifierz <m.slifierz@gmail.com>
2023-10-01 19:58:01 -0700

    changes

--- webapp/routers/country.py
+++ webapp/routers/country.py
@@ -22,13 +22,13 @@ def country_detail(request: Request, country_id: int, db: Session=Depends(get_db
     return templates.TemplateResponse("country_detail.html", {"request" : request, "country" : country})
 
 @router.get("/create-country")
-def create_country(reqest: Request):
-    return templates.TemplateResponse("create_country.html", {"request" : reqest})
+def create_country(request: Request):
+    return templates.TemplateResponse("create_country.html", {"request" : request})
 
 @router.post("/create-country")
-async def create_country(reqest: Request, db: Session=Depends(get_db)):
+async def create_country(request: Request, db: Session=Depends(get_db)):
     #take the form
-    form = await reqest.form()
+    form = await request.form()
     country_name  = form.get("country_name")
     capital = form.get("capital")
     official_lang = form.get("official_lang")
@@ -43,41 +43,42 @@ async def create_country(reqest: Request, db: Session=Depends(get_db)):
         errors.append("Description should be added ")
 
     if len(errors) > 0:
-        return templates.TemplateResponse("create_country.html", {"request" : reqest, "errors": errors})
+        return templates.TemplateResponse("create_country.html", {"request" : request, "errors": errors})
     
     # retrive the token from the cookie. 
     # the form title and browser data are send
 
     try:
- 
-        token = reqest.cookies.get("access_token" )
-        if token is None:
+        token = request.cookies.get("access_token")
+        if not token:
             errors.append("Create account or Login")
-            return templates.TemplateResponse("create_country.html", {"request" : reqest, "errors": errors})
-        else:
-            scheme, _, param = token.partition(" ")
-
+            return templates.TemplateResponse("create_country.html", {"request" : request, "errors": errors})
+        
+        scheme, _, param = token.partition(" ")
             #now decoded the token because i need the payload 
             # takes the email from the people who is owner of the post
-            payload = jwt.decode(param, setting.SECRET_KEY, algorithms=setting.ALGORITHM)
-            email = payload.get("sub")
+        payload = jwt.decode(param, setting.SECRET_KEY, algorithms=setting.ALGORITHM)
+        email = payload.get("sub")
             # take ID from database that is associate with this email
-            user = db.query(User.email==email).first()
+        if email is None:
+            errors.append("This user doesn't exist. Create account")
+            return templates.TemplateResponse("create_country.html", {"request" : request, "errors": errors})
+        else:
+            user = db.query(User).filter(User.email == email).first()
             if user is None:
-                errors.append("This user doesn't exist. Create account")
-                return templates.TemplateResponse("create_country.html", {"request" : reqest, "errors": errors})
+                errors.append("You are not authenticated, Kindly Login")
+                return templates.TemplateResponse( "create_item.html", {"request": request, "errors": errors})
             else:
                 # create object of the class
-                country = Country(country_name = country_name, capital =  capital, official_lang = official_lang, area = area,
-                                description = description, user_creator_id  = user.user_id)
+                country = Country(country_name=country_name, capital=capital, official_lang=official_lang, area=area,
+                                description=description, user_creator_id=user.user_id)
                 db.add(country)
                 db.commit()
                 db.refresh(country)
-                return responses.RedirectResponse("/detail/{country.country_id}", status.HTTP_302_FOUND)
-
-
-    
-     
+                return responses.RedirectResponse(f"/detail/{country.country_id}",status_code=status.HTTP_302_FOUND)
     except Exception as e:
+        errors.append("Something went wrong")
         print(e)
+        return templates.TemplateResponse(
+            "create_country.html", {"request": request, "errors": errors})
 
