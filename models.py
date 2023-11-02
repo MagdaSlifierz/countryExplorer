fcd88ce9190ba109079bd21901962e0b217e1e46
Magda Slifierz <m.slifierz@gmail.com>
2023-11-02 12:47:52 -0700

    image

--- models.py
+++ models.py
@@ -30,6 +30,7 @@ class Country(Base):
     official_lang = Column(String)
     area = Column(String)
     description = Column(String)
+    #addinf=g the image to coutry
     country_image = Column(String)
     
     user_creator_id = Column(Integer, ForeignKey("users.user_id"))
